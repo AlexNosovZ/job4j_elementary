14534e22be5ec88f8a386d136d82d6e00641fc0e
alex <zzlifnr@gmail.com>
2025-11-10 22:35:25 +0300

    1. Модульные тесты [#505277]

---
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.converter;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class ConverterTest {
+
+    @Test
+    void whenConvert180RblThen1dot9161Euro() {
+        double input = 180;
+        double expected = 1.9161;
+        double output = Converter.rubleToEuro(input);
+        double value = 0.0001;
+        assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+
+    @Test
+    void whenConvert9394RblThen100Euro() {
+        double input = 9394;
+        double expected = 100;
+        double output = Converter.rubleToEuro(input);
+        double value = 0.0001;
+        assertThat(output).isEqualTo(expected,withPrecision(value));
+    }
+
+    @Test
+    void whenConvert225RblThen3Dollar() {
+        double input = 225;
+        double expected = 2.7692;
+        double output = Converter.rubleToDollar(input);
+        double value = 0.0001;
+        assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+
+    @Test
+    void whenConvert8125RblThen100Dollar() {
+        double input = 8125;
+        double expected = 100;
+        double value = 0.0001;
+        double output = Converter.rubleToDollar(input);
+        assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+}
