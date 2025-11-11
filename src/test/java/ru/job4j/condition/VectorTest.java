8ac3f5f98a511f34ae5753f67c5a8d52b38634b0
alex <zzlifnr@gmail.com>
2025-11-11 22:53:21 +0300

    Тестирование Класса Сложения Векторов [#505262]

---
+++ src/test/java/ru/job4j/condition/VectorTest.java
@@ -0,0 +1,56 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class VectorTest {
+
+    @Test
+    void whenVectors12And34ThenResult45() {
+        int x1 = 1, y1 = 2, x2 = 3, y2 = 4;
+        String expected = "(4, 6)";
+        String output = Vector.addVectors(x1, y1, x2, y2);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenVectorsMinus12And34ThenResult22() {
+        int x1 = -1, y1 = -2, x2 = 3, y2 = 4;
+        String expected = "(2, 2)";
+        String output = Vector.addVectors(x1, y1, x2, y2);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenVectorsMinus12AndMinus34ThenResultMinus46() {
+        int x1 = -1, y1 = -2, x2 = -3, y2 = -4;
+        String expected = "(-4, -6)";
+        String output = Vector.addVectors(x1, y1, x2, y2);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenVectors00And00ThenResult00() {
+        int x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+        String expected = "(0, 0)";
+        String output = Vector.addVectors(x1, y1, x2, y2);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenVector00AndMinus12ThenResultMinus12() {
+        int x1 = 0, y1 = 0, x2 = -1, y2 = -2;
+        String expected = "(-1, -2)";
+        String output = Vector.addVectors(x1,y1,x2,y2);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenVector12AndMinus12ThenResult00() {
+        int x1 = 1, y1 = 2, x2 = -1, y2 = -2;
+        String expected = "(0, 0)";
+        String output = Vector.addVectors(x1,y1,x2,y2);
+        assertThat(output).isEqualTo(expected);
+    }
+}
