d00ddd76de5aeb0cf25d1592ee9d71704163f9aa
alex <zzlifnr@gmail.com>
2025-11-11 22:09:59 +0300

    Тестирование Класса Калькулятора Температуры Хранения [#505279]

---
+++ src/main/java/ru/job4j/calculator/TemperatureFit.java
@@ -0,0 +1,13 @@
+package ru.job4j.calculator;
+
+public class TemperatureFit {
+    public static double idealTemperatureForFruits(int temperature) {
+        // Ideal temperature for storing fruits in Celsius
+        return 8;
+    }
+
+    public static double idealTemperatureForMeat(int temperature) {
+        // Ideal temperature for storing meat in Celsius
+        return 5;
+    }
+}
